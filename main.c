8523b75a6d21a76e2565e957bfecc06fd9576be7
David Colon <dcolonantonio@gmail.com>
2025-05-15 00:08:44 -0400

    Add button modes functionality: implement color mode switching in buttonModes.c, create buttonModes.h for declarations, and update main.c to integrate button modes.

--- main.c
+++ main.c
@@ -1,9 +1,5 @@
 #include "pico/stdlib.h"
-#include "buttonModes.h"
-const int WHITE_LED_PIN = 15;
-const int YELLOW_LED_PIN = 14;
-const int BUTTON_PIN = 16;
+
 int main() {
-    runButtonModes(WHITE_LED_PIN, YELLOW_LED_PIN, BUTTON_PIN);
     return 0;
 }
